ca7c5b8809359441475e4b0ed13792f10bcf9112
SpicyRicecaker <awesomenessisgolden@gmail.com>
2023-09-09 22:13:56 -0700

    add debugging info

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,13 @@ use std::error::Error;
 use std::fmt::Write;
 use std::fs::{self, File};
 
+use log::info;
+
 const IMAGE_WIDTH: u32 = 256;
 const IMAGE_HEIGHT: u32 = 256;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    env_logger::init();
     // create a 256x256 image in ppm (lossless) format which goes from black to red for the first row (0->255)
     // then for every progressive row a green is added (0->255)
     let mut buf = String::new();
@@ -15,6 +18,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     for y in 0..IMAGE_HEIGHT {
         for x in 0..IMAGE_WIDTH {
+            info!("scanlines remaining: {}", IMAGE_HEIGHT - y);
             let r = x;
             let g = y;
             let b = 0;
